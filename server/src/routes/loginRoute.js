1874745ec0bf242cd84cfd9a0c19110463af8f42
Dominic Azuka <visitdominicazuka@gmail.com>
2023-05-12 06:20:15 +0100

    update

--- server/src/routes/loginRoute.js
+++ server/src/routes/loginRoute.js
@@ -1,14 +1,23 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const User = require("../models/user");
+import {AuthenticationDetails, CognitoUserPool, CognitoUserAttribute, CognitoUser} from 'amazon-cognito-identity-js';
+import {awsUserPool} from '../util/awsUserPool'
 
 export const logInRoute = {
   path: "/api/login",
   method: "post",
   handler: async (req, res) => {
     const { email, password: userPassword } = req.body;
-    const user = await User.findOne({ email });
+    new CognitoUserPool({Username:email, Pool: awsUserPool}).authenticateUser(new AuthenticationDetails({ Username: email, Password: password }), {
+      onSuccess: async (result) =>{
+        const user = await User.findOne({email});
+        
+      },
+      onFailure: (err) =>{
 
+      }
+    })
     if (!user) {
       return res.status(401).json({ message: "Unathorised!" });
     }
