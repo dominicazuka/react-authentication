821df1e3c79b24591ece6fa2dbc5f89b71b5da30
Dominic Azuka <visitdominicazuka@gmail.com>
2023-05-12 07:26:05 +0100

    update

--- server/src/routes/loginRoute.js
+++ server/src/routes/loginRoute.js
@@ -8,43 +8,33 @@ export const logInRoute = {
   path: "/api/login",
   method: "post",
   handler: async (req, res) => {
-    const { email, password: userPassword } = req.body;
-    new CognitoUserPool({Username:email, Pool: awsUserPool}).authenticateUser(new AuthenticationDetails({ Username: email, Password: password }), {
+    const { email, password } = req.body;
+    new CognitoUser({Username:email, Pool: awsUserPool}).authenticateUser(new AuthenticationDetails({ Username: email, Password: password }), {
       onSuccess: async (result) =>{
-        const user = await User.findOne({email});
+        const user = await User.findOne({ email });    
+    
+        const { _id: id, isVerified, info } = user;
         
+          jwt.sign(
+            {
+              id,
+              isVerified,
+              email,
+              info,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "3d" },
+            (err, token) => {
+              if (err) {
+                return res.sendStatus(500);
+              }
+              res.status(200).json({ token });
+            }
+          );
       },
       onFailure: (err) =>{
-
+        res.sendStatus(401);
       }
     })
-    if (!user) {
-      return res.status(401).json({ message: "Unathorised!" });
-    }
-
-    const { _id: id, isVerified, password, info } = user;
-
-    const isCorrect = await bcrypt.compare(userPassword, password);
-
-    if (isCorrect) {
-      jwt.sign(
-        {
-          id,
-          isVerified,
-          email,
-          info,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "3d" },
-        (err, token) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-          res.status(200).json({ token });
-        }
-      );
-    } else{
-        res.sendStatus(401)
-    }
   },
 };
